75d2f46d0c0484c74ef34ea421b81f7ead22113e
Soyam01 <shresthasoyam256@gmail.com>
2026-06-18 11:30:59 +0545

    added counter

---
+++ src/hooks/Counter.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Counter = () => {
+  return (
+    <div className='h-[80vh] flex justify-center items-center text-4xl flex-col'>
+        Count: {count}
+
+        <button onClick={() => {
+
+        }} className='bg-blue-500 hover:bg-blue-300 active:bg-blue-600 p py-2 uppercase'>
+
+        </button>
+    </div>
+  )
+}
+
+export default Counter
